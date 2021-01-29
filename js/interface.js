ccd93316633c29493011531513306cb0708870df
AndrRyaz <andriy.r@upplabs.com>
2021-01-29 09:09:35 +0200

    Added condition for Nan value

--- js/interface.js
+++ js/interface.js
@@ -315,6 +315,10 @@
 
   function isNewerVersion(latestVersionMenu, currentMenuVersion) {
     for (var index = 0; index < latestVersionMenu.length; index++) {
+      if (isNaN(+latestVersionMenu[index])) latestVersionMenu[index] = 0;
+
+      if (isNaN(+currentMenuVersion[index])) currentMenuVersion[index] = 0;
+
       if (+latestVersionMenu[index] > +currentMenuVersion[index]) return true;
 
       if (+latestVersionMenu[index] < +currentMenuVersion[index]) return false;
